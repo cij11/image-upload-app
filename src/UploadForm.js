9e5e8d15aba83a7110536e3a1caa2256af73489e
Chris Jolly <chrisjolly25@gmail.com>
2018-07-24 21:30:14 +1200

    Initial commit image upload project.

---
+++ src/UploadForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UploadForm extends Component {
+    render() {
+        return (
+            <div>
+                Yo Yo wassup
+            </div>
+        );
+    }
+}
+
+export default UploadForm;
